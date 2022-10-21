fcfebf4f60c3373fbe50a09e34c2617395b7b99d
SpinoTech <haldarparichay@gmail.com>
2022-10-21 10:57:14 +0530

    first commit

---
+++ link_to_QR.js
@@ -0,0 +1,14 @@
+const main_box=document.querySelector(".main_box");
+let btn=document.querySelector(".btn");
+let get_input=document.querySelector(".from input");
+let qr_code=document.querySelector(".show_QR img")
+// const main_box=document.getElementsByClassName("main_box");
+// btn=document.getElementsByClassName("btn");
+
+btn.addEventListener("click",()=>{
+  let val=get_input.value;
+if(!val)return;
+// btn.innerText="Generating QR Code...";
+// getting the qr code from google API 
+qr_code.src=` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${val}`;
+});
